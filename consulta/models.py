97a85923b4f6c83c3cea1946f09dc497ec0ced3d
iam-eduardamoreira <moreira.eduarda@estudante.ifmt.edu.br>
2025-02-26 13:00:30 -0400

    Atualizações na tabela Consulta

--- consulta/models.py
+++ consulta/models.py
@@ -1,14 +1,16 @@
 from django.db import models
+from paciente.models import Paciente
+from profissional.models import Profissional
 
 class Consulta(models.Model):
     id_consulta = models.AutoField(primary_key=True)
-    dt_consulta = models.DateTimeField(auto_now_add=True)
+    dt_consulta = models.DateField(auto_now_add=True)
     hr_consulta = models.TimeField(auto_now_add=True)
     id_profissional = models.ForeignKey(
         'profissional.Profissional',
         models.DO_NOTHING,
         db_column='id_profissional',
-        to_field='id_profissional'  # Garante que a FK usa o campo correto
+        to_field='id_profissional'  #garante que a FK usa o campo correto
     )
     id_paciente = models.ForeignKey(
         'paciente.Paciente',
